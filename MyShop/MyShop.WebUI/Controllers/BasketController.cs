9c87f65541b327423c6339f27a258407455fc5bd
andrew.jones <andrew.jones@neas.nhs.uk>
2019-01-11 14:28:57 +0000

    Added Order Creation v2.

--- MyShop/MyShop.WebUI/Controllers/BasketController.cs
+++ MyShop/MyShop.WebUI/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +11,12 @@ namespace MyShop.WebUI.Controllers
     public class BasketController : Controller
     {
         IBasketService basketService;
+        IOrderService orderService;
 
-        public BasketController(IBasketService BasketService)
+        public BasketController(IBasketService BasketService, IOrderService OrderService)
         {
             this.basketService = BasketService;
+            this.orderService = OrderService;
         }
         // GET: Basket
         public ActionResult Index()
@@ -41,6 +44,55 @@ namespace MyShop.WebUI.Controllers
             var basketSummary = basketService.GetBasketSummary(this.HttpContext);
 
             return PartialView(basketSummary);
+        }
+
+        //  [Authorize]
+        public ActionResult Checkout()
+        {
+            //Customer customer = customers.Collection().FirstOrDefault(c => c.Email == User.Identity.Name);
+
+            //if (customer != null)
+            //{
+            //    Order order = new Order()
+            //    {
+            //        Email = customer.Email,
+            //        City = customer.City,
+            //        State = customer.State,
+            //        Street = customer.Street,
+            //        Firstname = customer.FirstName,
+            //        Surname = customer.LastName,
+            //        ZipCode = customer.ZipCode
+            //    };
+
+                return View(/*order*/);
+            //}
+            //else
+            //{
+            //    return RedirectToAction("Error");
+            //}
+        }
+
+        [HttpPost]
+        //  [Authorize]
+        public ActionResult Checkout(Order order)
+        {
+            var basketItems = basketService.GetBasketItems(this.HttpContext);
+            order.OrderStatus = "Order Created";
+            //order.Email = User.Identity.Name;
+
+            //process payment
+
+            order.OrderStatus = "Payment Processed";
+            orderService.CreateOrder(order, basketItems);
+            basketService.ClearBasket(this.HttpContext);
+
+            return RedirectToAction("Thankyou", new { OrderId = order.Id });
+        }
+
+        public ActionResult ThankYou(string OrderId)
+        {
+            ViewBag.OrderId = OrderId;
+            return View();
         }
     }
 }
